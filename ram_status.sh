c1f5cd64f740f49ce524b413a53a16cc582b6c88
Juhee Lavanya <juheelavanya@gmail.com>
2026-01-30 04:59:23 +0000

    Improve system check logic and cleanup project structure

--- ram_status.sh
+++ ram_status.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+LOG_FILE="$SCRIPT_DIR/ram_monitor.log"
+
 FREE_SPACE=$(free -mt | grep "Total" | awk '{print $4}')
 
 MAX_LIMIT=5000
 
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
 if [[ $FREE_SPACE -lt $MAX_LIMIT ]]
 then
-	echo "WARNING! RAM is running low. "
+	echo "$TIMESTAMP WARNING! RAM is running low."|tee -a "$LOG_FILE"
 else
-	echo "Sufficient RAM sapce available - $FREE_SPACE M"
+	echo "$TIMESTAMP Sufficient RAM sapce available - $FREE_SPACE M"|tee -a "$LOG_FILE"
 fi
 
 
